b9f6073d31bd1a866814ace38f871ae4a2d35add
wajiha-majeed <wajihamajeed44@gmail.com>
2024-03-24 23:12:00 +0500

    Add project files.

---
+++ LMS/Models/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LMS.Models;
+
+public partial class Library
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Author { get; set; }
+
+    public decimal? Price { get; set; }
+}
---
+++ LMS/Models/LmsContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Models;
+
+public partial class LmsContext : DbContext
+{
+    public LmsContext()
+    {
+    }
+
+    public LmsContext(DbContextOptions<LmsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Library> Libraries { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=SYEDTAHMEEDALI;Database=lms;Trusted_Connection=True;trustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Library>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__LIBRARY__3214EC0718F1599A");
+
+            entity.ToTable("LIBRARY");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Author)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("AUTHOR");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("NAME");
+            entity.Property(e => e.Price)
+                .HasColumnType("money")
+                .HasColumnName("PRICE");
+            entity.Property(e => e.Title)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("TITLE");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
